321b0834b2cd85b056c15945d2ff6673aca6dd19
ereshk86 <145262591+ereshk86@users.noreply.github.com>
2023-09-17 16:57:45 +0530

    Add files via upload

---
+++ practice/arrays/PrintAlternateOrder.java
@@ -0,0 +1,16 @@
+package practice.arrays;
+
+public class PrintAlternateOrder {
+	public static void main(String[] args) {
+		int inputArray[] = { 1, 2, 3, 4, 5 };
+		int lenght = inputArray.length;
+		printAlterateOrder(inputArray, lenght);
+	}
+
+	private static void printAlterateOrder(int[] inputArray, int lenght) {
+		for (int i = 0; i < inputArray.length; i = i + 2) {
+			System.out.println(inputArray[i]);
+		}
+	}
+
+}
