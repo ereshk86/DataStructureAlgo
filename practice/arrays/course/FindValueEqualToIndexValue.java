8ce29b4ad9e0af21a7f24b03cc9eef5904b7632a
ereshk86 <145262591+ereshk86@users.noreply.github.com>
2023-09-19 22:45:34 +0530

    Add files via upload

---
+++ practice/arrays/course/FindValueEqualToIndexValue.java
@@ -0,0 +1,23 @@
+package practice.arrays;
+
+public class FindValueEqualToIndexValue {
+	public static void main(String[] args) {
+		int nums[] = { 15, 2, 45, 12, 7 };
+		int size = nums.length;
+		int opValue = findValueEqualToIndexValue(nums, size);
+		if (opValue == 0) {
+			System.out.println("No Match");
+		} else {
+			System.out.println(opValue);
+		}
+	}
+
+	private static int findValueEqualToIndexValue(int[] nums, int size) {
+		for (int i = 0; i < nums.length; i++) {
+			if (i+1 == nums[i])
+				return nums[i];
+		}
+		return 0;
+	}
+
+}
