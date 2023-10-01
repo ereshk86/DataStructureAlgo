dc43f9837f3e9774b81374924b23d09da984b7bc
ereshk86 <145262591+ereshk86@users.noreply.github.com>
2023-10-01 12:13:58 +0530

    Add files via upload

---
+++ practice/arrays/leetcode/MoveZeroes.java
@@ -0,0 +1,24 @@
+package practice.arrays.leetcode;
+
+public class MoveZeroes {
+	public static void main(String[] args) {
+		int[] arr = { 0, 3, 0, 2, 4 };
+		moveZeroes(arr);
+	}
+
+	public static void moveZeroes(int[] nums) {
+		int i = 0;
+		for (final int num : nums) {
+			if (num != 0) {
+				nums[i++] = num;
+			}
+		}
+		while (i < nums.length) {
+			nums[i++] = 0;
+		}
+		for (int i1 = 0; i1 < nums.length; i1++) {
+			System.out.println(nums[i1]);
+		}
+	}
+
+}
