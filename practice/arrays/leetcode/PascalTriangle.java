89bc0fff498656645d6c06ea20c79427e76d0916
ereshk86 <145262591+ereshk86@users.noreply.github.com>
2023-10-05 23:56:45 +0530

    Add files via upload

---
+++ practice/arrays/leetcode/PascalTriangle.java
@@ -0,0 +1,34 @@
+package practice.arrays.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+	public static void main(String[] args) {
+		int numRows = 5;
+		List<ArrayList<Integer>> result = generate(numRows);
+		System.out.println(result);
+	}
+
+	private static List<ArrayList<Integer>> generate(int numRows) {
+
+		List<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		if (numRows <= 0)
+			return result;
+		ArrayList<Integer> first = new ArrayList<Integer>();
+		first.add(1);
+		result.add(first);
+		for (int i = 1; i < numRows; i++) {
+			ArrayList<Integer> before = result.get(i - 1);
+			ArrayList<Integer> now = new ArrayList<Integer>();
+			now.add(1);
+			for (int j = 1; j < i; j++) {
+				now.add(before.get(j - 1) + before.get(j));
+			}
+			now.add(1);
+			result.add(now);
+		}
+		return result;
+	}
+
+}
