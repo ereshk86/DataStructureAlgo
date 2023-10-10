deefbb604cefc7f09f7702360039979c3bf24f03
ereshk86 <145262591+ereshk86@users.noreply.github.com>
2023-10-10 23:13:18 +0530

    Add files via upload

---
+++ practice/arrays/leetcode/RangeSumQuery.java
@@ -0,0 +1,29 @@
+package practice.arrays.leetcode;
+
+public class RangeSumQuery {
+	public static void main(String[] args) {
+		int nums[] = { -2, 0, 3, -5, 2, -1 };
+		NumArray numsObj = new NumArray(nums);
+		int range = numsObj.sumRange(0, 5);
+		System.out.println(range);
+	}
+
+}
+
+class NumArray {
+	private int[] input;
+
+	public NumArray(int[] nums) {
+		input = new int[nums.length];
+		System.arraycopy(nums, 0, input, 0, nums.length);
+	}
+
+	public int sumRange(int left, int right) {
+		int temp = 0;
+		for (int i = left; i <= right; i++) {
+			temp = temp + input[i];
+		}
+		return temp;
+
+	}
+}
