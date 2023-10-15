05f72414a802d63e45f78bb41a16a4d72654b1f6
ereshk86 <145262591+ereshk86@users.noreply.github.com>
2023-10-15 23:26:31 +0530

    Add files via upload

---
+++ practice/arrays/leetcode/BinarySubArrayWithSum.java
@@ -0,0 +1,32 @@
+package practice.arrays.leetcode;
+
+public class BinarySubArrayWithSum {
+	public static void main(String[] args) {
+		int N = 5;
+		int target = 2;
+		int arr[] = { 1, 0, 1, 0, 1 };
+
+		int n = numberOfSubArrays(arr, N, target);
+		System.out.println(n);
+	}
+
+	private static int numberOfSubArrays(int[] arr, int N, int target) {
+		return fun(arr, N, target) - fun(arr, N, target - 1);
+	}
+
+	private static int fun(int[] arr, int N, int target) {
+		int sum = 0, count = 0;
+		int left = 0, right = 0;
+
+		while (right < N) {
+			sum += arr[right];
+			while (sum > target) {
+				sum -= arr[left];
+				left++;
+			}
+			count += (right - left + 1);
+			right++;
+		}
+		return count;
+	}
+}
