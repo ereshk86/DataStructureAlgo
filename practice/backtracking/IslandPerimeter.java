a43c63ea8a1f43d56477c996e7959d79d1684a6c
ereshk86 <145262591+ereshk86@users.noreply.github.com>
2023-12-04 22:09:03 +0530

    Add files via upload

---
+++ practice/backtracking/IslandPerimeter.java
@@ -0,0 +1,31 @@
+package practice.backtracking;
+
+public class IslandPerimeter {
+
+	public static void main(String[] args) {
+		int grid[][] = { { 0, 1, 0, 0 }, { 1, 1, 1, 0 }, { 0, 1, 0, 0 }, { 1, 1, 0, 0 } };
+		int res = islandPerimeter(grid);
+		System.out.println(res);
+	}
+
+	private static int islandPerimeter(int[][] grid) {
+
+		int islands = 0;
+		int neighbors = 0;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == 1) {
+					++islands;
+					if (i - 1 >= 0 && grid[i - 1][j] == 1)
+						neighbors++;
+					if (j - 1 >= 0 && grid[i][j] == 1)
+						neighbors++;
+
+				}
+			}
+		}
+
+		return islands * 4 - neighbors * 2;
+	}
+
+}
